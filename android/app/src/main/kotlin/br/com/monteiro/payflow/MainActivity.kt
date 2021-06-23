9f66f6fdbb346ad691198c5b51364d79f84e3f66
Matheus Monteiro <matheus.stack.20@gmail.com>
2021-06-23 14:58:11 -0300

    SplashPage, LoginPage e Inicio da HomePage

---
+++ android/app/src/main/kotlin/br/com/monteiro/payflow/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.monteiro.payflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: SplashPage(),
+    );
+  }
+}
